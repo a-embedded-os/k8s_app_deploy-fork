f9d0c5ce11d5150be99a6da70abc397c11b8169f
Jeremy J. Miller <jeremymv2@users.noreply.github.com>
2022-01-11 11:39:00 -0500

    adding full URL to commit_sha annotation
    Signed-off-by: Jeremy J. Miller <jeremymv2@users.noreply.github.com>

--- jenkins/webapp01/util/deploy.sh
+++ jenkins/webapp01/util/deploy.sh
@@ -7,6 +7,7 @@ cd "$1"
 for f in k8s/*yaml; do
   echo "Getting commit SHA for: $f"
   export COMMIT_SHA="$(git log -n 1 --all --pretty=format:%H -- $f)"
+  export GITHUB_SHA_URL="https://github.com/$2/$3/commit/$COMMIT_SHA"
   envsubst < "$f" | kubectl apply -f -
 done
 
