5f74c69cae38e7a1dd0aca42e4d875fdd9e25531
Jeremy J. Miller <jeremymv2@users.noreply.github.com>
2022-01-19 14:10:52 -0500

    update App.js
    Signed-off-by: Jeremy J. Miller <jeremymv2@users.noreply.github.com>

--- jenkins/webapp01/src/App.js
+++ jenkins/webapp01/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Howdy SDR
+        Howdy SDR!
         </p>
         <a
           className="App-link"
