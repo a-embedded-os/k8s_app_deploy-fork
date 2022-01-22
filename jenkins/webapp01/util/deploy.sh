a24c6b1d95ad4a2f0b77fd1a6fdd9ed66695358e
Jeremy J. Miller <jeremymv2@users.noreply.github.com>
2022-01-22 08:25:08 -0500

    distinguish between version bump and release events
    Signed-off-by: Jeremy J. Miller <jeremymv2@users.noreply.github.com>

--- jenkins/webapp01/util/deploy.sh
+++ jenkins/webapp01/util/deploy.sh
@@ -2,13 +2,22 @@
 
 set -x
 
-cd "$1"
+cd "$1" || exit 1
+export COMMIT_SHA GITHUB_SHA_URL
 
 for f in k8s/*yaml; do
   echo "Getting commit SHA for: $f"
-  export COMMIT_SHA="$(git log -n 1 --all --pretty=format:%H -- $f)"
-  export GITHUB_SHA_URL="https://github.com/$2/$3/commit/$COMMIT_SHA"
-  envsubst < "$f" | kubectl apply -f -
+  COMMIT_SHA=$(git log -n 1 --all --pretty=format:%H -- "$f")
+  GITHUB_SHA_URL="https://github.com/$2/$3/commit/$COMMIT_SHA"
+  envsubst < "$f" | kubectl diff -f -
+  if [[ "$?" -eq 1 ]]; then
+    echo "[INFO] Differences exist between local and online configuration. Applying local config.."
+    envsubst < "$f" | kubectl apply -f -
+    kubectl rollout restart deploy/"$4"
+  elif [[ "$?" -gt 1 ]]; then
+    echo "[ERROR] Diff between local and online configuration failed!"
+  else
+    echo "[INFO] No differences detected between online configuration and local."
+  fi
 done
 
-kubectl rollout restart deploy/$4
