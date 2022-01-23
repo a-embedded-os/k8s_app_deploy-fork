fdbebb5c088b8f955b3759b3b5f74ce78cb133b0
Jeremy J. Miller <jeremymv2@users.noreply.github.com>
2022-01-22 22:13:43 -0500

    add separate k8s cluster contexts
    Signed-off-by: Jeremy J. Miller <jeremymv2@users.noreply.github.com>

--- jenkins/webapp01/util/deploy.sh
+++ jenkins/webapp01/util/deploy.sh
@@ -11,22 +11,29 @@ function owner_repo {
 
 function maybe_deploy {
   for f in "$SCRIPTPATH"/../k8s/*/*yaml; do
+    env_dir="$(dirname "$f")"
     echo ""
     echo "========================================"
-    echo "| Processing manifests in $(dirname "$f")"
+    echo "| Processing manifests in $env_dir"
     echo "| Getting commit SHA for: $f"
     echo "========================================"
     echo ""
     COMMIT_SHA=$(git log -n 1 --all --pretty=format:%H -- "$f")
     GITHUB_SHA_URL="https://github.com/$(owner_repo)/commit/$COMMIT_SHA"
     # shellcheck disable=1091
-    [[ -f $(dirname "$f")/.env ]] && source "$(dirname "$f")/.env"
-    envsubst < "$f" | kubectl diff -f - 2>&1
+    if [[ -f $env_dir/.env ]]; then
+      echo "[INFO] Sourcing .env file with Environment Variable override contents:"
+      echo "============"
+      cat "$env_dir/.env"
+      echo "============"
+      source "$env_dir/.env"
+    fi
+    envsubst < "$f" | kubectl diff --kubeconfig "$2" --context "$K8S_CONTEXT" -f - 2>&1
     if [[ "$?" -eq 1 ]]; then
       echo "[INFO] Differences exist between local and online configuration. APPLYING!"
-      envsubst < "$f" | kubectl apply -f - 2>&1
+      envsubst < "$f" | kubectl apply --kubeconfig "$2" --context "$K8S_CONTEXT" -f - 2>&1
       echo "[INFO] Running a rollout restart for Deployment."
-      kubectl rollout restart deploy/"$1" 2>&1
+      kubectl rollout restart deploy/"$1" --kubeconfig "$2" --context "$K8S_CONTEXT" 2>&1
     elif [[ "$?" -gt 1 ]]; then
       echo "[ERROR] Diff between local and online configuration FAILED!"
     else
