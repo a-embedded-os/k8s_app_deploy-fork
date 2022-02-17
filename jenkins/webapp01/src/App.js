e85a07dca5bbeb7e87f942055dfdb8aa8e17e575
Jeremy J. Miller <jeremymv2@users.noreply.github.com>
2022-02-17 12:20:08 -0500

    fix: welcome message
    Signed-off-by: Jeremy J. Miller <jeremymv2@users.noreply.github.com>

--- jenkins/webapp01/src/App.js
+++ jenkins/webapp01/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Howdy Platform9!
+        Howdy Fortnight!
         </p>
         <a
           className="App-link"
