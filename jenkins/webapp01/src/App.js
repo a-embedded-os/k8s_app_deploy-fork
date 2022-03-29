1cba6fa38f8337ca35f71a96eb8aa930c81acdd8
mike.riggs <mike.riggs@pf9-0069.lan>
2022-03-29 10:30:47 -0500

    Edited title

--- jenkins/webapp01/src/App.js
+++ jenkins/webapp01/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hello Mike!
+        Hello Dish!
         </p>
         <a
           className="App-link"
